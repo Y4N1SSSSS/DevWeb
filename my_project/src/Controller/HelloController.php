67a986417ffac3807ca15c6c10a3028aacb51975
Y4N1SSSSS <153291185+Y4N1SSSSS@users.noreply.github.com>
2024-02-07 17:13:19 +0100

    Changement dans le premier controller et création du fichier base.html.twig et du fichier bonjour.html.twig

--- my_project/src/Controller/HelloController.php
+++ my_project/src/Controller/HelloController.php
@@ -1,15 +1,23 @@
 <?php
-
 namespace App\Controller;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
-
-    public function sayHello(): Response
+    #[Route('/hello', name: 'app_hello')]
+    public function sayHello()
     {
-        return new Response("Coucou");
+        return new Response('Hello!');
+    }
+
+    #[Route('/bonjour/{nom}',name: 'app_bonjour')]
+    public function bonjour($nom)
+    {
+        //return new Response("Bonjour $nom !");
+        return $this->render('bonjour.html.twig', [
+        'nom' => $nom,
+        ]);
     }
 }
